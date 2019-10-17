27ad18ee2c00cb24bcdb3b81474c3fa7546a676e
FCraven <craven.francis@gmail.com>
2019-10-17 11:25:46 -0400

    drawPaddle function added

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 const canvas = document.getElementById('myCanvas');
 const ctx = canvas.getContext('2d');
 let x = canvas.width/2;
-let y = canvas.height-30;
+let y = canvas.height - 30;
 let dx = 2;
 let dy = -2;
 const ballRadius = 10
-const colorArray = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+const paddleHeight = 10;
+const paddleWidth = 75;
+const paddleX = (canvas.width-paddleWidth) / 2
 
-function changeColor() {
-  ctx.fillStyle = colorArray[Math.round(Math.random() * colorArray.length)]
-  ctx.fill()
-}
+
 
 function drawBall() {
   ctx.beginPath();
@@ -20,25 +19,26 @@ function drawBall() {
   ctx.closePath();
 }
 
+function drawPaddle() {
+  ctx.beginPath()
+  ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
+  ctx.fillStyle = '#0095DD'
+  ctx.fill()
+  ctx.closePath()
+}
+
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   drawBall();
-
-
-  if(y + dy > canvas.height -ballRadius || y + dy < ballRadius){
-    changeColor()
-    console.log(ctx.fillStyle)
-    dy = -dy
-  }
-
-  if(x + dx > canvas.width - ballRadius || x + dx < ballRadius){
-    changeColor()
-    console.log(ctx.fillStyle)
-    dx = -dx;
-  }
-
+  drawPaddle();
+    if(y + dy > canvas.height - ballRadius || y + dy < ballRadius){
+      dy = -dy
+    }
+    if(x + dx > canvas.width - ballRadius || x + dx < ballRadius){
+      dx = -dx;
+    }
   x += dx;
   y += dy;
 }
 
-setInterval(draw, 10)
+setInterval(draw, 20)
